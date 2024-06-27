76288bce91cbae1977585ede4d79838460a5aae0
ManaHive <manazranwastaken@gmail.com>
2024-06-27 13:12:25 +0800

    Began work on Highest Salary bar chart

--- pages/3_📋_Descriptive.py
+++ pages/3_📋_Descriptive.py
@@ -6,6 +6,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_lottie import st_lottie 
 import lottie_animations as l
+import plotly_express as plt
 
 st.set_page_config(
     page_title = "Descriptive",
@@ -17,10 +18,8 @@ st.set_page_config(
 @st.cache_data
 def fetch_data():
     df = pd.read_csv("processed_data.csv")
-    st.dataframe(df)
-
     return df
-
+df = fetch_data()
 #create columns for a side-by-side layout of the text and lottie animation
 col1, col2 = st.columns([1,1])
 
@@ -28,31 +27,37 @@ col1, col2 = st.columns([1,1])
 with col1:
     st.subheader("**Part 1: Descriptive Models page**")
     st.write("Here, our descriptive models will be displayed, that aim to provide insight on past patterns regarding salary trends in the data science industry, including:")
-    st.markdown("* Average Salary by Experience Level and Employment Type")
     st.markdown("* Highest Salary by Profession")
     st.markdown("* Job Market Saturation of Top 5-10 Highest Salary Professions")
     st.markdown("* Correlation between Company Size and Salary")
+    st.markdown("* Average Salary by Experience Level and Employment Type")
 #Display lottie animation in the second column
 with col2:
     st_lottie(l.descriptive_lottie, loop = True, width = 600, height = 350, key = None)
 #tabs for different desc models 
-tab1, tab2, tab3, tab4 = st.tabs(["Average Salary", 
-                                  "Highest Salary Profession", 
+tab1, tab2, tab3, tab4 = st.tabs(["Highest Salary Profession", 
                                   "Job Market Saturation",
-                                  "Company Size vs Salary"])
+                                  "Company Size vs Salary",
+                                  "Average Salary"])
 
 with tab1:
-    st.markdown("#### 🧑 *Average Salary by Experience Level and Employment Type*")
+    st.markdown("#### 📈 *Highest Salary by Profession (Top 15)*")
     st.divider()
 
+    high_salary = df.groupby(["job_title"])["salary_in_usd"].max().head(15).sort_values(ascending=False)
+    fig = plt.bar(high_salary)
+    st.plotly_chart(fig)
+
 with tab2:
-    st.markdown("#### 📈 *Highest Salary by Profession*")
+    st.markdown("#### 🗺️ *Job Market Saturation of Top 15 Highest Salary Professions*")
     st.divider()
 
 with tab3:
-    st.markdown("#### 🗺️ *Job Market Saturation of Top 5-10 Highest Salary Professions*")
-    st.divider()
-
-with tab4:
     st.markdown("#### 🏢 *Correlation between Company Size and Salary*")
     st.divider()
+    
+
+with tab4:
+    st.markdown("#### 🧑 *Average Salary by Experience Level and Employment Type*")
+    st.divider()
+    
